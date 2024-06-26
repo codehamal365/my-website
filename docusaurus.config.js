68a98117c9088e4e4c15008a2b2831743e0ca58b
Code hamal <30389683+codehamal365@users.noreply.github.com>
2024-06-26 17:22:47 +0800

    try

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'codehamal365', // Usually your GitHub org/user name.
-  projectName: 'my_website', // Usually your repo name.
+  projectName: 'my-website', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
